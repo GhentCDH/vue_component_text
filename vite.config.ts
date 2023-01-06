395aeed383c4e56dd1151f5dd3039c21330e25f4
Pieterjan De Potter <pieterjan.depotter@ugent.be>
2023-01-06 20:52:38 +0100

    Configure build including type definitions

--- vite.config.ts
+++ vite.config.ts
@@ -1,20 +1,36 @@
 /// <reference types="vitest" />
-import { fileURLToPath, URL } from "node:url";
 
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
+import { resolve } from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  resolve: {
-    alias: {
-      "@": fileURLToPath(new URL("./src", import.meta.url)),
-    },
-  },
   test: {
     coverage: {
       reportsDirectory: "../test_coverage",
     },
+  },
+  build: {
+    lib: {
+      entry: resolve(__dirname, "src/components/index.ts"),
+      name: "VueComponentText",
+      formats: ["es", "cjs"],
+      fileName: (format) => {
+        if (format === "es") return "vue-component-text.es.js";
+        if (format === "cjs") return "vue-component-text.cjs";
+        return "";
+      },
+    },
+    rollupOptions: {
+      external: ["vue"],
+      output: {
+        globals: {
+          vue: "Vue",
+        },
+      },
+    },
+    outDir: "dist",
   },
 });
