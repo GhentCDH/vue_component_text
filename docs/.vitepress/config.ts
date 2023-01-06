c823584e5a6c633c180287b50aa10959bc51f8ae
Pieterjan De Potter <pieterjan.depotter@ugent.be>
2023-01-06 13:46:48 +0100

    Improve documentation (add edit and social links)

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -1,6 +1,22 @@
+import { defineConfig } from "vitepress";
+
 const { BASE: base = "/" } = process.env;
-export default {
-  title: "vue_component_text",
+
+export default defineConfig({
+  title: "Text Block",
   description: "Text display component.",
   base,
-};
+  themeConfig: {
+    editLink: {
+      pattern:
+        "https://github.com/GhentCDH/vue_component_text/edit/main/docs/:path",
+      text: "Edit this page on GitHub",
+    },
+    socialLinks: [
+      {
+        icon: "github",
+        link: "https://github.com/GhentCDH/vue_component_text",
+      },
+    ],
+  },
+});
