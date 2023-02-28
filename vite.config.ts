373e7e101b0355b2199371f9937334f6cfab9e73
Pieterjan De Potter <pieterjan.depotter@ugent.be>
2023-02-28 11:23:07 +0100

    Fix typing information in build

--- vite.config.ts
+++ vite.config.ts
@@ -3,15 +3,16 @@
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 import { resolve } from "path";
+import dts from "vite-plugin-dts";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
-  test: {
-    coverage: {
-      reportsDirectory: "../test_coverage",
-    },
-  },
+  plugins: [
+    vue(),
+    dts({
+      copyDtsFiles: true,
+    }),
+  ],
   build: {
     lib: {
       entry: resolve(__dirname, "src/components/index.ts"),
@@ -32,5 +33,10 @@ export default defineConfig({
       },
     },
     outDir: "dist",
+  },
+  test: {
+    coverage: {
+      reportsDirectory: "../test_coverage",
+    },
   },
 });
